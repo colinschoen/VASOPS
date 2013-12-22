9e6d49abc5d49d1f98392d27dfde2fd63ad1eb1a
colinschoen <colin@colinschoen.com>
2013-12-21 18:21:39 -0800

    Start of clicks calculations

--- app/controllers/VaController.php
+++ app/controllers/VaController.php
@@ -4,6 +4,13 @@ class VaController extends BaseController {
     public function get_va() {
         //Pull our users VA data
         $record = User::where('cid', '=', Auth::user()->cid)->first();
+
+        //Figure out what the last 4 months are
+        $month = date('m');
+        $month1before = date('m', strtotime('-1 month'));
+        $month2before = date('m', strtotime('-2 month'));
+        $month3before = date('m', strtotime('-3 month'));
+
         //Create our view with the record
         return View::make('va')->with(array('record' => $record));
     }
