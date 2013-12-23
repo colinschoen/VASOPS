10eee8d1f3505d9cc141225bfa6429daa31e8c98
colinschoen <colin@colinschoen.com>
2013-12-22 20:11:56 -0800

    -Tickets model -Tickets template -Tickets controller logic -Tickets database structure

--- app/models/Clicks.php
+++ app/models/Clicks.php
@@ -4,4 +4,10 @@ class Clicks extends Eloquent {
 
     public $table = 'clicks';
 
+    //We want to use unix time stamps for this table
+    protected function getDateFormat()
+    {
+        return time();
+    }
+
 }
