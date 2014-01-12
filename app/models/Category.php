7d351285411a3349b41a9af8cac69434e5e06dc3
colinschoen <colin@colinschoen.com>
2014-01-12 00:48:56 -0800

    Current VAs progress

--- app/models/Category.php
+++ app/models/Category.php
@@ -5,4 +5,28 @@ class Category extends Eloquent {
     public $table = 'categories';
     public $timestamps = FALSE;
 
+
+    static public function isParent($id) {
+        $category = Category::where('id', '=', $id)->first();
+        if ($category->parentid == 0) {
+            return TRUE;
+        }
+        else {
+            return FALSE;
+        }
+    }
+
+    static public function getChildren($id) {
+        $categories = Category::where('parentid', '=', $id)->get();
+        $children = array();
+        foreach ($categories as $category) {
+            $children[] = $category->name;
+        }
+        return $children;
+    }
+
+    static public function countChildren($id) {
+        $numChildren = Category::where('parentid', '=', $id)->count();
+        return $numChildren;
+    }
 }
