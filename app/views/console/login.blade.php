22bddc1e8a8125184c60d8175e557fefbea3d1a6
colinschoen <colin@colinschoen.com>
2014-01-25 10:37:10 -0800

    Console dashboard push

--- app/views/console/login.blade.php
+++ app/views/console/login.blade.php
@@ -1,32 +1,34 @@
 @include('console.core.header')
-<div class="row" style="margin-bottom: 40px;">
-    <div class="col-md-8 col-md-offset-3">
-        <h1><strong>VATSIM VA Auditor Console</strong></h1>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-4 col-md-offset-4">
-        @if (Session::get('message') != '')
-        <div class="alert alert-warning">
-        {{ Session::get('message') }}
+<div class="container">
+    <div class="row" style="margin-bottom: 40px;">
+        <div class="col-md-8 col-md-offset-3">
+            <h1><strong>VATSIM VA Auditor Console</strong></h1>
         </div>
-        @endif
-        <div class="login-panel panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Please sign in</h3>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            @if (Session::get('message') != '')
+            <div class="alert alert-warning">
+            {{ Session::get('message') }}
             </div>
-            <div class="panel-body">
-                <form action="{{ URL::route('postconsolelogin') }}" method="POST" role="form">
-                    <fieldset>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="CID" name="cid" value="{{ Session::get('cid') }}" autofocus>
-                        </div>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="Password" name="password" type="password" value="">
-                        </div>
-                        <input type="submit" class="btn btn-lg btn-success btn-block" value="Login" />
-                    </fieldset>
-                </form>
+            @endif
+            <div class="login-panel panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Please sign in</h3>
+                </div>
+                <div class="panel-body">
+                    <form action="{{ URL::route('postconsolelogin') }}" method="POST" role="form">
+                        <fieldset>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="CID" name="cid" value="{{ Session::get('cid') }}" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                            </div>
+                            <input type="submit" class="btn btn-lg btn-success btn-block" value="Login" />
+                        </fieldset>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
