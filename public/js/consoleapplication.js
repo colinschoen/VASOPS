2fd24a3ade86192c8a81f2ad136876b639ff259a
colinschoen <colin@colinschoen.com>
2014-06-27 16:51:29 -0700

    Update status page to add actions

---
+++ public/js/consoleapplication.js
@@ -0,0 +1,17 @@
+$('.tooltip-bottom').tooltip({
+    placement: 'bottom',
+});
+$('.tooltip-top').tooltip({
+    placement: 'top',
+});
+$('.tooltip-right').tooltip({
+    placement: 'right',
+});
+$('.tooltip-top').tooltip({
+    placement: 'left',
+});
+
+smoothScroll.init({
+    speed: 850,
+    easing: 'easeInOutQuad',
+});
