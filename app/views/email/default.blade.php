0df99c8a801408d890eb12311653e2ab1b536c29
colinschoen <colin@colinschoen.com>
2014-06-29 01:38:54 -0700

    Major progress on console

---
+++ app/views/email/default.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>VATSIM Virtual Airline System 1.0</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="span3">
+                    <h2 class="demo-logo">VATSIM VA System <small>1.0</small></h2>
+                    <div style="margin-top: 10px;"></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="span12">
+                    <div class="well">
+                        {{ $content }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
